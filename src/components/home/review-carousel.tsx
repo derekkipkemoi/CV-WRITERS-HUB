27748fb8be9cbf0f90c57d60d9f394c90fbead23
Kipkemoi Derrick <derekkipkemoi@gmail.com>
2024-11-19 17:33:10 +0300

    Changes

--- src/components/home/review-carousel.tsx
+++ src/components/home/review-carousel.tsx
@@ -169,12 +169,9 @@ Our clients appreciate our dedication to crafting exceptional resumes that help 
 
     <div className="review-carousel">
     <Slider
-        {...{
-          ...settings,
-          customPaging: (i) => <Dots slideCount={reviews.length} currentSlide={i} goToSlide={() => { 
-            // Intentionally left empty
-          }} />,
-        }}
+        {
+          ...settings
+        }
       >
         {reviews.map((review, index) => (
           <div key={index}>
