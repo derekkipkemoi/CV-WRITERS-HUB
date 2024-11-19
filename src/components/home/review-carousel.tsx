44767bfbc8213620b1724556514dd1d732a68c3f
Kipkemoi Derrick <derekkipkemoi@gmail.com>
2024-11-19 18:19:49 +0300

    Changes

--- src/components/home/review-carousel.tsx
+++ src/components/home/review-carousel.tsx
@@ -82,7 +82,7 @@ const settings = {
   infinite: true,
   speed: 500,
   autoplay: true,
-  autoplaySpeed: 3000, 
+  autoplaySpeed: 4000, 
   slidesToShow: 3,
   slidesToScroll: 1,
   nextArrow: <CustomNextArrow />,
